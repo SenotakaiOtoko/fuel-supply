b30f8810746aa0094a48747db264e44de5f84ee8
Rybakov Viktor <V.Rybakov@hostco.ru>
2019-12-17 16:22:14 +0500

    init commit

---
+++ src/test/java/ru/urfu/tapochek/fuelsupply/FuelSupplyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.tapochek.fuelsupply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelSupplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
